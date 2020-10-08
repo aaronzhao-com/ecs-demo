3ddd92ff8b77f55603008a06cecbe9f5428839ff
Aaron Zhao <aaron.zhao@myob.com>
2020-10-08 22:31:57 +1100

    buildkite and cfn to deploy network

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ecs-demo
+
+
+Reference: https://docs.aws.amazon.com/codebuild/latest/userguide/cloudformation-vpc-template.html
---
+++ ops/scripts/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -euo pipefail
+
+stackup () {
+    docker run --rm -v "`pwd`:/cwd"  -v "$HOME/.aws_${ENV}:/root/.aws" -e AWS_DEFAULT_REGION realestate/stackup:${STACKUP_VERSION} "$@"
+}
+
+stackup ${ENV}-ecs-demo-${STACK} up \
+    --preserve-template-formatting \
+    -t ./deploy/cfn/${STACK}.yaml \
+    -p ./deploy/cfn/params/${ENV}-${STACK}.yaml
