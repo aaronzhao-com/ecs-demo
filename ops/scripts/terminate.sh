b22aeb7495612e4f4873e54877305fca616a12e1
Aaron Zhao <aaron.zhao@myob.com>
2020-10-08 23:16:15 +1100

    fix  termination

--- ops/scripts/terminate.sh
+++ ops/scripts/terminate.sh
@@ -5,7 +5,4 @@ stackup () {
     docker run --rm -v "`pwd`:/cwd"  -v "$HOME/.aws_${ENV}:/root/.aws" -e AWS_DEFAULT_REGION realestate/stackup:${STACKUP_VERSION} "$@"
 }
 
-stackup ${ENV}-ecs-demo-${STACK} delete \
-    --preserve-template-formatting \
-    -t ./ops/cfn/${STACK}.yaml \
-    -p ./ops/cfn/params/${ENV}-${STACK}.yaml
+stackup ${ENV}-ecs-demo-${STACK} delete
