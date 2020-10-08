37509440afa1e1cc6b2a3b0cb98c2b831ed6bb32
Aaron Zhao <aaron.zhao@myob.com>
2020-10-08 23:29:33 +1100

    moved stackup version setting to shell script

--- ops/scripts/terminate.sh
+++ ops/scripts/terminate.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
+STACKUP_VERSION='1.5.0@sha256:fa1bc4eac1ec9c80a5aca4b3bc420b063c110392413bd89886fd0d8f0c1e94f2'
+
 stackup () {
-    docker run --rm -v "`pwd`:/cwd"  -v "$HOME/.aws_${ENV}:/root/.aws" -e AWS_DEFAULT_REGION realestate/stackup:${STACKUP_VERSION} "$@"
+    docker run --rm -v "`pwd`:/cwd"  -v "$HOME/.aws_${ENV}:/root/.aws" -e AWS_DEFAULT_REGION realestate/stackup:$STACKUP_VERSION "$@"
 }
 
 stackup ${ENV}-ecs-demo-${STACK} delete
